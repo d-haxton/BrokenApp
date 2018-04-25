54089369f8ec91518ad787e118e6b7a65b38ef58
David Haxton <haxton.dc@gmail.com>
2018-04-24 19:05:58 -0700

    BrokenApp

---
+++ BrokenApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SQLite;
+using System.Windows;
+
+namespace BrokenApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var connection = new SQLiteConnection("Data Source=brokenapp.db;Version=3;foreign keys=true;DateTimeKind=Utc");
+            connection.Open();
+            connection.EnableExtensions(true);
+            try
+            {
+                connection.LoadExtension("BrokenAppNative.dll", "empty_extension_this_should_fail");
+            }
+            catch (SQLiteException)
+            {
+                // should throw sqlite exception as its an empty library
+            }
+
+            new MainWindow().Show();
+        }
+    }
+}
